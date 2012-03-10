29bc22990256e73d44b3ab899204d7b2f52762f9
Kalle <carlpetter.bertell@gmail.com>
2012-03-10 20:21:56 +0200

    orientation

--- src/com/phunt/Phunt.java
+++ src/com/phunt/Phunt.java
@@ -2,6 +2,7 @@ package com.phunt;
 
 import org.apache.cordova.DroidGap;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Phunt extends DroidGap
@@ -9,6 +10,7 @@ public class Phunt extends DroidGap
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
         super.loadUrl("file:///android_asset/www/index.html");
     }
 }
